f18ead7d544af84fba8ab7784bef7a991effb292
ksj58884 <ksj58884@gmail.com>
2024-07-30 00:30:07 +0900

    🚚 코드카타 매일매일 작성

---
+++ src/main/java/com/sparta/code/level1/Solution24.java
@@ -0,0 +1,24 @@
+package com.sparta.code.level1;
+
+class Solution24 {
+    
+    public int solution(int n, int m, int[] section) {
+        int start = section[0];
+        int end = section[0] + (m - 1);
+        int ans = 1;
+        
+        for (int i : section) {
+            if (i >= start && i <= end) {
+                continue;
+            } 
+            else {
+                start = i;
+                end = i + (m - 1);
+                ans++;
+            }
+        }
+        
+        return ans;
+    }
+    
+}
