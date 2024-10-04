0b5b60c473722232e0e847028418a57d9e4cf01d
ksj58884 <ksj58884@gmail.com>
2024-10-04 23:59:02 +0900

    🚚 코드카타 매일매일 작성

---
+++ src/main/java/com/sparta/code/level2/Solution67.java
@@ -0,0 +1,40 @@
+package com.sparta.code.level2;
+
+import java.util.ArrayList;
+
+class Solution67 {
+    
+    public int[] solution(int n, long k) {
+        int[] answer = new int[n];
+        ArrayList<Integer> ary = new ArrayList<>();
+        
+        for (int i = 1; i <= n; i++) {
+            ary.add(i);
+        }
+        
+        long total = 1;
+        for (int i = 1; i <= n; i++) {
+            total *= i;
+        }
+        
+        k--;
+        
+        int idx = 0;
+        while (idx < n) {
+            total /= (n - idx);
+            answer[idx++] = ary.remove((int) (k / total));
+            k %= total;
+        }
+        
+        return answer;
+    }
+    
+    public static void main(String[] args) {
+        Solution67 sol = new Solution67();
+        int n = 3;
+        long k = 4;
+        int[] result = sol.solution(n, k);
+        System.out.println(Arrays.toString(result));
+    }
+    
+}
