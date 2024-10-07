7aa0e0dab47118628f0521a58a21fea80cf7da9a
ksj58884 <ksj58884@gmail.com>
2024-10-07 23:55:59 +0900

    🚚 코드카타 매일매일 작성

---
+++ src/main/java/com/sparta/code/level2/Solution69.java
@@ -0,0 +1,64 @@
+package com.sparta.code.level2;
+
+import java.util.*;
+
+class Solution69 {
+    
+    public String[] solution(String[][] plans) {
+        LinkedList<Task> tasks = new LinkedList<>();
+        
+        for (String[] plan : plans) {
+            tasks.offer(new Task(plan[0], convertToMinute(plan[1]), Integer.parseInt(plan[2])));
+        }
+        tasks.sort((t1, t2) -> t1.start - t2.start);
+
+        Stack<Task> stopTasks = new Stack<>();
+        List<String> endTasks = new ArrayList<>();
+        Task now = tasks.poll();
+        int time = now.start;
+        
+        while (!tasks.isEmpty()) {
+            time += now.left;
+            Task next = tasks.peek();
+
+            if (time > next.start) {
+                now.left = time - next.start;
+                stopTasks.push(now);
+            } else {
+                endTasks.add(now.name);
+                if (!stopTasks.empty()) {
+                    now = stopTasks.pop();
+                    continue;
+                }
+            }
+            now = tasks.poll();
+            time = now.start;
+        }
+
+        endTasks.add(now.name);
+        
+        while (!stopTasks.empty()) {
+            endTasks.add(stopTasks.pop().name);
+        }
+
+        return endTasks.toArray(new String[0]);
+    }
+
+    private int convertToMinute(String time) {
+        String[] t = time.split(":");
+        return Integer.parseInt(t[0]) * 60 + Integer.parseInt(t[1]);
+    }
+
+    class Task {
+        String name;
+        int start;
+        int left;
+
+        Task(String name, int start, int left) {
+            this.name = name;
+            this.start = start;
+            this.left = left;
+        }
+    }
+    
+}
