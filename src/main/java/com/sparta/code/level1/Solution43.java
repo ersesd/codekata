7a77949efdc6c4fa00da4cda3a72199139cd2515
ksj58884 <ksj58884@gmail.com>
2024-10-23 23:48:33 +0900

    🚚 코드카타 매일매일 작성

---
+++ src/main/java/com/sparta/code/level1/Solution43.java
@@ -0,0 +1,15 @@
+package com.sparta.code.level1;
+
+class Solution43 {
+    
+    public int[] solution(int[] num_list) {
+        int[] reversed = new int[num_list.length];
+        
+        for (int i = 0; i < num_list.length; i++) {
+            reversed[i] = num_list[num_list.length - 1 - i];
+        }
+        
+        return reversed;
+    }
+    
+}
