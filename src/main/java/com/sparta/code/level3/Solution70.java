1ffde43f017e5edfe6a449878b799901c30ec1b2
ksj58884 <ksj58884@gmail.com>
2025-02-23 02:41:12 +0900

    🚚 코드카타 매일매일 작성

---
+++ src/main/java/com/sparta/code/level3/Solution70.java
@@ -0,0 +1,27 @@
+package com.sparta.code.level3;
+
+public class Solution70 {
+    
+    public int solution(int n, int[][] computers) {
+        boolean[] visited = new boolean[n];
+        int networkCount = 0;
+        
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                networkCount++;
+                dfs(i, computers, visited, n);
+            }
+        }
+        return networkCount;
+    }
+    
+    private void dfs(int curr, int[][] computers, boolean[] visited, int n) {
+        visited[curr] = true;
+        for (int j = 0; j < n; j++) {
+            if (computers[curr][j] == 1 && !visited[j]) {
+                dfs(j, computers, visited, n);
+            }
+        }
+    }
+    
+}
